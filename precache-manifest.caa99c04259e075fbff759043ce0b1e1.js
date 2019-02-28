384f876845c7a95245365723793406af1c6c9219
Adham Khalilieh <adham@megatam.net>
2019-02-28 16:12:59 +0200

    prod

---
+++ precache-manifest.caa99c04259e075fbff759043ce0b1e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "883ce335f463f37db12a3c81c6a3b6f8",
+    "url": "/static/media/bubble_back.883ce335.png"
+  },
+  {
+    "revision": "ee2f074aa624df391fac",
+    "url": "/static/css/main.2dc7ae5a.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3ce5f572e0dbd994d7a7ae18b442a164",
+    "url": "/static/media/dots-full.3ce5f572.png"
+  },
+  {
+    "revision": "6a9e95ae74d29ea38cfd",
+    "url": "/static/js/2.6a9e95ae.chunk.js"
+  },
+  {
+    "revision": "9e297a5c0d60937b8a5a1acf140c482d",
+    "url": "/static/media/Loading.9e297a5c.gif"
+  },
+  {
+    "revision": "ee2f074aa624df391fac",
+    "url": "/static/js/main.ee2f074a.chunk.js"
+  },
+  {
+    "revision": "81de3104e42ecc3b79aa38e00d4dab49",
+    "url": "/static/media/desk.81de3104.jpg"
+  },
+  {
+    "revision": "df37bf582931d9f109ada156e3639b7f",
+    "url": "/static/media/newback.df37bf58.png"
+  },
+  {
+    "revision": "1897faa2e15b9c96f905ff88b296980d",
+    "url": "/static/media/dots.1897faa2.png"
+  },
+  {
+    "revision": "6a9e95ae74d29ea38cfd",
+    "url": "/static/css/2.fa6fbcd3.chunk.css"
+  },
+  {
+    "revision": "fc8b51f318893c930b76ecb5b72352f7",
+    "url": "/index.html"
+  }
+];
